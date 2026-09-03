679e4639e43c0d418e491cb881d112445e335f49
rwijbenga <rwijbenga@users.noreply.github.com>
2026-09-03 20:19:12 +0200

    Fix ClimateTraits for ESPHome 2026.5+ feature flags API.

--- components/mhi_xy/mhi_xy.cpp
+++ components/mhi_xy/mhi_xy.cpp
@@ -433,7 +433,7 @@ climate::ClimateTraits MhiXyClimate::traits() {
       climate::CLIMATE_SWING_OFF,
       climate::CLIMATE_SWING_VERTICAL,
   });
-  traits.set_supports_current_temperature(true);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
   traits.set_visual_min_temperature(16.0f);
   traits.set_visual_max_temperature(30.0f);
   traits.set_visual_temperature_step(0.5f);
